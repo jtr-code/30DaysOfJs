53e166ae5328138e57b6b3a4ded2db5b85a6a2e7
Jishnu T Raj <jtrtricks@gmail.com>
2022-10-09 15:06:37 +0530

    tested all variables again

---
+++ 00_Variables.js
@@ -0,0 +1,17 @@
+var a=20;
+var a=10;
+console.log(a);
+var wa=100;
+console.log(wa);
+
+{
+
+    var wa=67;
+ 
+    console.log(wa)
+}
+var wa="helloi";
+console.log(wa);
+const num=45;
+console.log(num);
+const num2;//produce error bcoz const must be initialized during declaration.
