17701d79b573d99523a1d0835a545885e5e9a51e
jtrtricks <jtrtricks@gmail.com>
2022-10-12 12:49:04 +0530

    update to operator and expressions

---
+++ 01_Operator_and_Expression.js
@@ -0,0 +1,20 @@
+let a=5;
+let b=1;
+
+// console.log("a+b: ",a+b);
+// console.log("a-b: ",a-b);
+// console.log("a/b: ",a/b);
+// console.log("a*b: ",a*b);
+// console.log("a**b:",a**b);
+// console.log("a++: ",a++);
+// console.log("++a: ",++a);
+// console.log("a--: ",a--);
+// console.log("--a: ",--a);
+b=++a;
+console.log("a:",a)
+console.log("b:",b);
+b=a++;
+console.log("a:",a)
+console.log("b:",b);
+console.log("b:",b);
+
