650c27ef7351b16e7d3eae3373f0053ac183d887
jtrtricks <jtrtricks@gmail.com>
2022-10-12 21:53:03 +0530

    switch case

---
+++ switch.js
@@ -0,0 +1,25 @@
+const prompt=require("prompt-sync")();
+let foo = prompt("pick a no from 1 to 5: ");
+foo=Number.parseInt(foo);
+let output = 'Output: ';
+switch (foo) {
+  case 0:
+    output += 'So ';
+  case 1:
+    output += 'What ';
+    output += 'Is ';
+  case 2:
+    output += 'Your ';
+  case 3:
+    output += 'Name';
+  case 4:
+    output += '?';
+    console.log(output);
+    break;
+  case 5:
+    output += '!';
+    console.log(output);
+    break;
+  default:
+    console.log('Please pick a number from 0 to 5!');
+}
