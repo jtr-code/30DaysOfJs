aff68b699dc7ed04481f0b96de18b499deaf3cfd
jtrtricks <jtrtricks@gmail.com>
2022-10-15 11:30:13 +0530

    Strings added

---
+++ 05_Strings.js
@@ -0,0 +1,34 @@
+//Strings are used to store and manipulate the text.
+
+let name = 'jishnu';
+
+console.log(name);
+
+let name1 = "jtr";
+
+console.log(name1);
+
+//////////template literals////////////////////////
+
+let place = "kerala";
+
+let number = 9393939393;
+
+let address = `my name is jishnu I am from ${place} and my Phone number is ${number}`;
+
+console.log(address);
+
+//Template Literals are used to define the string using BACKTICS instead of Quotes.
+
+//////////Escape Character Sequence/////////////////
+
+// let name3='Adam D' Angelo';
+// console.log(name3); error
+
+//we can use single quote escape sequence to solve the problem.
+
+let name3='Adam D\' Angelo';
+console.log(name3);
+
+let name4="Adam D\' Angelo1";
+console.log(name4);
