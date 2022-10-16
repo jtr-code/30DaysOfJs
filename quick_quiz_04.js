3cd118d4d3d1ccda7c6ea2b9b62155138df1d8fb
jtrtricks <jtrtricks@gmail.com>
2022-10-16 14:27:14 +0530

    Updated quick_quiz_04.js

--- quick_quiz_04.js
+++ quick_quiz_04.js
@@ -1,4 +1,4 @@
-//Program to print all the element in the array.
+//Program to print all the element in the array one by one.
 
 let arr=['123','Jishnu','Ajay','Anurag'];
 for(i=0;i<arr.length;i++){
