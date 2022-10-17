b0fd2b45dd9e41822b27c86832badbd839e82324
jtrtricks <jtrtricks@gmail.com>
2022-10-17 21:01:47 +0530

    update to 11_Array_Loops.js

--- 11_Array_Loops.js
+++ 11_Array_Loops.js
@@ -1,14 +1,18 @@
 let arr = [11, 12, 13, 14, 15];
-for (i = 0; i <= arr.length; i++) {
+for (i = 0; i < arr.length; i++) {
     console.log(arr[i]);
 }
 
-for (let item in arr) {
-    console.log(arr[item]);
+//////////////////////for_in_and_for_of_loop/////////////////////////////////////////////////////////////////////
+
+let list = [4, 5, 6];
+
+for (let i in list) {
+    console.log(i); // "0", "1", "2",[keys]
 }
 
-for (let i of arr) {
-    console.log(i);
+for (let i of list) {
+    console.log(i); // "4", "5", "6",[values]
 }
 
 /////////////////////////////////////forEach/////////////The forEach() method calls a function and iterates over the elements of an array.
@@ -20,15 +24,3 @@ num.forEach((element) => {
 
 })
 console.log(num);//[1, 2, 3, 4, 5]
-
-////////////////////////for_in_and_for_of_loop/////////////////////////////////////////////////////////////////////
-
-let list = [4, 5, 6];
-
-for (let i in list) {
-    console.log(i); // "0", "1", "2",[keys]
-}
-
-for (let i of list) {
-    console.log(i); // "4", "5", "6",[values]
-}
