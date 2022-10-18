82db8700a78a5c98e3584cc2cb2cbe188ba4090b
jtrtricks <jtrtricks@gmail.com>
2022-10-18 16:04:48 +0530

    Added qns

--- quick_quiz_05.js
+++ quick_quiz_05.js
@@ -21,13 +21,33 @@ console.log(numArray);
 //Keeping adding numbers to the array until 0 is added to the array./////////////////////////////
 
 let num = [1, 2, 3, 4, 5];
-
-
 let a;
-do{
-     a=prompt("Enter a number: ");
-    a=Number.parseInt(a);
+do {
+    a = prompt("Enter a number: ");
+    a = Number.parseInt(a);
     num.push(a);
 
-}while(a!=0);
-console.log(num)
+} while (a != 0);
+
+console.log(num);
+
+//Filter the numbers that are divisible by 10 from a given array.///////////////////////////////////////////////////
+
+let arr = [20, 30, 1, 4, 55, 40];
+
+let divisible = ((e) => {
+    return e % 10 == 0;
+})
+
+let filteredResult = arr.filter(divisible);
+
+console.log(filteredResult);//[ 20, 30, 40 ]
+
+//Create an array of square of given numbers.//////////////////////////////////////////////////////////////////////////
+
+let newArr = [1, 3, 5, 7, 9];
+ 
+let squareArr=newArr.map((e)=>{
+    return e*e;
+})
+console.log(squareArr);//[ 1, 9, 25, 49, 81 ]
