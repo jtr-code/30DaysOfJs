23e6ee4f5d81093006eea63a6bde39d581fae9b8
jtr-code <jtrtricks@gmail.com>
2022-10-26 21:03:11 +0530

    Initial commmit

---
+++ 17_Callback.js
@@ -0,0 +1,53 @@
+// const name = ((data) => {
+//     console.log("Place "+data);
+// })
+
+// const place = (callback) =>{
+//     callback("kerala")
+// }
+
+// place(name);
+
+// let sum = ((a, b) => {
+//     sum = a + b
+//     console.log(sum);
+// })
+
+// let variables = ((cb) => {
+//     cb(5, 2);
+// })
+
+// variables(sum)
+
+
+console.log("Start");
+
+function getDataFromApi(callback) {
+    setTimeout(() => {
+        callback("Jishnu t raj");
+    }, 2000);
+}
+
+
+function getDataFromApi2(callback) {
+    setTimeout(() => {
+        callback(" is");
+    }, 2000);
+}
+function getDataFromApi3(callback) {
+    setTimeout(() => {
+        callback(" op");
+    }, 2000);
+}
+// console.log(getDataFromApi());
+
+getDataFromApi((value) => {
+    getDataFromApi2((api2val) => {
+        // console.log(value + api2val);
+        getDataFromApi3((api3val) => {
+            console.log(value + api2val + api3val);
+        })
+    })
+})
+
+console.log("end");
