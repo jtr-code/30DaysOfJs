56c5c5d5a679e148d99cfe0cd9dd0bf02b2363d5
jtr-code <jtrtricks@gmail.com>
2022-10-27 12:34:38 +0530

    Added promise

---
+++ 18_Promises.js
@@ -0,0 +1,27 @@
+//The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+
+
+// let resPromise = new Promise((resolve, reject) => {
+//     console.log("Promise 1 is pending..");
+//     resolve("I am resolved");
+
+// })
+
+// let rejPromise = new Promise((resolve, reject) => {
+//     console.log("Promise 2 is pending.. ");
+//     reject("Rejected");
+// })
+
+// resPromise.then(() => {
+//     console.log("fullfulled promise 1")
+// })
+
+// rejPromise.then(() => {
+//     console.log("I am not resolved promise 2");
+
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+
+
