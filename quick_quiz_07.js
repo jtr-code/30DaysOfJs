4fc4079fed64227c2073b45771cafb60a973515a
jtr-code <jtrtricks@gmail.com>
2022-10-29 13:54:15 +0530

    Updated

--- quick_quiz_07.js
+++ quick_quiz_07.js
@@ -1,43 +1,44 @@
 // Program to load a js file using Promises and use .then() to display an alert() when the load is complete.//////////////////////////////////
 
-// const loadScript = (src) => {
-//     return new Promise((resolve, reject) => {
-//         let script = document.createElement('script');
-//         script.src = src;
-//         script.onload = () => {
-//             resolve(src + "It is solved");
-//         }
-//         document.head.append(script)
-//     })
+const loadScript = (src) => {
+    return new Promise((resolve, reject) => {
+        let script = document.createElement('script');
+        script.src = src;
+        script.onload = () => {
+            resolve(src + "It is solved");
+        }
+        document.head.append(script)
+    })
 
-// }
+}
 
-// let a = loadScript('https://jsonplaceholder.typicode.com/todos/1');
-// a.then((value) => {
-//     console.log(value);
-//     alert("Loaded")
-// }).catch((err) => {
-//     console.log(err)
-// })
+
+let a = loadScript('https://jsonplaceholder.typicode.com/todos/1');
+a.then((value) => {
+    console.log(value);
+    alert("Loaded")
+}).catch((err) => {
+    console.log(err)
+})
 
 
 ///////////////////////////////Use the async await with above problem///////////////////////////////////////////////////////////////////////////////
 
-// const loadScript = async (src) => {
-//     return new Promise((resolve, reject) => {
-//         let script = document.createElement('script');
-//         script.src = src;
-//         script.onload = () => {
-//             resolve("Loaded Script" + src);
-//         }
-//         document.head.append(script);
-//     })
-// }
-// let mainFunc = async () => {
-//     let a = await value('https://jsonplaceholder.typicode.com/todos/1');
-//     console.log(a);
-// }
-// mainFunc();
+const loadScript = async (src) => {
+    return new Promise((resolve, reject) => {
+        let script = document.createElement('script');
+        script.src = src;
+        script.onload = () => {
+            resolve("Loaded Script" + src);
+        }
+        document.head.append(script);
+    })
+}
+let mainFunc = async () => {
+    let a = await value('https://jsonplaceholder.typicode.com/todos/1');
+    console.log(a);
+}
+mainFunc();
 
 //////////Create a promise which reject after 3 seconds use an async/await to get its value.Use a try/catch to handle the error.//////////
 
@@ -57,5 +58,43 @@ const a = async () => {
 }
 a();
 
+//Write a Program using Promise.all() inside an async/await to await 3 promises.Compare its result with the case where we await these promises one by one.
 
+console.time()
+const promise0 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Its solved at 2 seconds");
+    }, 2000); 
+})
 
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Its solved at 3 seconds");
+    }, 3000);
+})
+
+const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Its solved at 4 seconds");
+    }, 4000);
+})
+
+const value = async () => {
+    // console.log("start")
+    // // let promise0Value = await promise0;
+    // // let promise1Value = await promise1;
+    // // let promise2Value = await promise2;
+    // // console.log(promise0Value, promise1Value, promise2Value);//it tooks 4+seconds
+    // // console.log("end")
+    console.log("start")
+    let promise0Value =  promise0;
+    let promise1Value =  promise1;
+    let promise2Value =  promise2;
+    let promiseAll =await Promise.all([promise0Value, promise1Value, promise2Value]);//it took 0.561ms
+    console.log(promiseAll);
+    console.log("end")
+    console.timeEnd();
+
+}
+
+value();
