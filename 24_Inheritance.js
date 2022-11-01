8e44ad662dc5b6be938d536ac72b813a846f3437
jtr-code <jtrtricks@gmail.com>
2022-11-02 00:13:02 +0530

    Added Inheritance

---
+++ 24_Inheritance.js
@@ -0,0 +1,26 @@
+
+//Inheritance is useful for code reusability: reuse properties and methods of an existing class when you create a new class.
+
+class Parent {
+    constructor(name, place) {
+        this.name = name;
+        this.place = place;
+    }
+    displayDetails() {
+        console.log(`The name is ${this.name} and place is ${this.place}`);
+
+    }
+}
+
+class Child extends Parent {
+
+    displayAdditional() {
+        console.log("Cute boi");
+    }
+}
+
+let obj1 = new Parent("Jishnu", "kerala");
+let obj2 = new Child();
+
+obj1.displayDetails();
+obj2.displayAdditional();
