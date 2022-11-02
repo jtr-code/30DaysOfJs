25d8f407c98b32f7226e6b8906c218d38ce43334
jtr-code <jtrtricks@gmail.com>
2022-11-02 15:40:15 +0530

    Updated

---
+++ 26_Static_Method.js
@@ -0,0 +1,15 @@
+//Static class methods are defined on the class itself.
+// You cannot call a static method on an object, only on an object class.
+
+class Student {
+    constructor(name) {
+        this.name = name;
+        console.log("Student name is " + name + " and age is " + Student.studentAge(5));
+    }
+    static studentAge(age) {
+        return age;
+
+    }
+}
+
+let obj1 = new Student("jtr", Student.studentAge(5));
